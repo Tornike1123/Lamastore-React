ba5a2afa54fe7dfc5e65fa32b8578b47df6d99dd
Tornike1123 <toko36106@gmail.com>
2022-12-18 18:04:07 +0400

    Products Page - Filtering Markup

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -2,7 +2,49 @@ import React from "react";
 import "./Product.scss";
 
 const Product = () => {
-	return <div className="product">Product</div>;
+	return (
+		<div className="product">
+			<div className="left">
+				<div className="filterItem">
+					<h2>Product Categories</h2>
+					<div className="inputItem">
+						<input type="checkbox" id="1" value={1} />
+						<label htmlFor="1">Shoes</label>
+					</div>
+					<div className="inputItem">
+						<input type="checkbox" id="2" value={2} />
+						<label htmlFor="2">Skirt</label>
+					</div>
+					<div className="inputItem">
+						<input type="checkbox" id="3" value={3} />
+						<label htmlFor="3">Coats</label>
+					</div>
+				</div>
+				<div className="filterItem">
+					<h2>Filter By Price</h2>
+					<div className="inputItem">
+						<span>0</span>
+						<input type="range" min={0} max={100} />
+						<span>100</span>
+					</div>
+				</div>
+				<div className="filterItem">
+					<h2>Sort by</h2>
+					<div className="inputItem">
+						<input type="radio" id="asc" value="asc" name="price" />
+						<label htmlFor="asc">Lowest Price</label>
+					</div>
+					<div className="inputItem">
+						<input type="radio" id="asc" value="asc" name="price" />
+						<label htmlFor="asc">Highest Price</label>
+					</div>
+				</div>
+			</div>
+			<div className="right">
+				<div className="catImg"></div>
+			</div>
+		</div>
+	);
 };
 
 export default Product;
